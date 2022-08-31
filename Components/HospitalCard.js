54f93e4dc7412b4e86d45490870e1df1a23b5d87
zay <damilolaoyeniyi13@gmail.com>
2022-08-31 20:36:07 +0100

    minor tweaks

--- Components/HospitalCard.js
+++ Components/HospitalCard.js
@@ -4,31 +4,31 @@ import Link from 'next/link'
 
 export default function HavannaCard(props){
     return(
-        <Box maxW='xl' h='140px' bgGradient='linear(to-r, #23a9ff, #48a7ba)' marginTop='40px' borderRadius='20px' display='flex'  boxShadow='5px 7px #d0cdcd'>
+        <Box maxW='xl' h='140px' bgGradient='linear(to-r, #23a9ff, #48a7ba)' marginTop='40px' borderRadius='20px' display='flex'  boxShadow=' 0 10px 20px -5px #848b90' >
               <Image src={`${props.img}`}  alt="Havana specialist hospital" maxW='8rem' p="30px"/>
               <Box display='flex' flexDirection='column'>
                 <Box display='flex'>
-                  <Heading fontSize='2xl' color='white' marginTop='20px' marginLeft='25px'>{props.name}</Heading>
+                  <Heading fontSize='2xl' color='white' marginTop='20px' >{props.name}</Heading>
                 </Box>
-                <Box display='flex' alignItems='center' mt='5px' ml='27px'>
-                  <Text color='white'>{props.stats.rating}</Text>
-                  <StarIcon ml='5px' color='#f0ca67'/>
-                  <StarIcon ml='1.5px'color='#f0ca67'/>
-                  <StarIcon ml='1.5px' color='#f0ca67'/>
-                  <StarIcon ml='1.5px' color='#f0ca67'/>
-                  <StarIcon ml='1.5px' color='lightgrey'/>
+                <Box display='flex' alignItems='center' mt='5px' >
+                  {Array(5)
+                  .fill('')
+                  .map((_, i) => (
+                    <StarIcon
+                      key={i}
+                      color={i < props.stats.rating ? '#ffba23' : 'gray.300'}
+                    />
+                   ))}
                   <Text color='white' ml='10px'>{props.stats.reviewCount} Reviews</Text>
                 </Box>
-                <Box ml='12px' display='flex' mt='5px'>
+                <Box ml='-10px' display='flex' mt='5px'>
                   <TriangleDownIcon ml='10px' mt='5px' color='red'/>
                   <Text fontSize='15px' color='white' wordBreak='normal'>{props.address}</Text>
                 </Box>
 
                 <Box display='block' ml='380px' mt='-60px' >
                 <InfoOutlineIcon color='white'/>
-                  <Link href="../pages/Hospitals/Havanna/HavannaInfo">
                     <Text color='white' ml='-25px' textDecoration='underline' cursor='pointer'>More info?</Text>
-                  </Link>
                 </Box>
               </Box>
               {/* {Array(5)
--- Components/hospitalCardDataa.js
+++ Components/hospitalCardDataa.js
@@ -4,7 +4,7 @@ export default [
         img: "https://www.havanaspecialisthospital.com/media/img/logo_xy.png",
         name: "Havana specialist hospital",
         stats: {
-            rating: 4.1,
+            rating: 4.0,
             reviewCount: 30
         },
         address: "115, Akerele Extension, Surulere Lagos"
@@ -24,7 +24,7 @@ export default [
         img: "https://reddingtonhospital.com/wp-content/uploads/2020/02/REDDINGTON-LOGO-FOR-WEBSITE-1-1-300x110-1.png",
         name: "Reddington Hospital",
         stats: {
-            rating: 3.2,
+            rating: 3.0,
             reviewCount: "121"
         },
         address: "12, Ajanaku Street, Opebi, Ikeja, Lagos"
@@ -54,7 +54,7 @@ export default [
         img: "https://www.ettaatlantic.com/wp-content/uploads/2018/12/Etta-Atlantic-Memorial-Hospital-logo.png",
         name: "Etta-Atlantic Memorial Hospital",
         stats: {
-            rating: 4.4,
+            rating: 4.0,
             reviewCount: 37
         },
         address: "2 Abioro St, Ikate 106104, Lekki, Lagos"
