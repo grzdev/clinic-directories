b34d73f36cac055aa1a69a3055010f3ec0434ac8
zay <damilolaoyeniyi13@gmail.com>
2022-09-11 10:52:10 +0100

    bug fix

--- next.config.js
+++ next.config.js
@@ -4,15 +4,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/account123/**',
-      },
-    ],
-  },
-}
+module.exports = nextConfig
