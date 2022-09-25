bdbac1917a37aff0029d2bbe0c120bb3d62d0617
zay <damilolaoyeniyi13@gmail.com>
2022-09-25 20:00:59 +0100

    bug fix

--- redux/store.js
+++ redux/store.js
@@ -2,7 +2,7 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import cardReducer from './slices/cardSlice';
 
 
-const combineReducers = combineReducers({
+const reducer = combineReducers({
     card: cardReducer
 })
 
