7bd62d794271530693818629851d2471e7b9f4b6
zay <damilolaoyeniyi13@gmail.com>
2022-09-25 19:58:11 +0100

    store fix

--- redux/store.js
+++ redux/store.js
@@ -1,20 +1,12 @@
-import {
-    configureStore
-} from '@reduxjs/toolkit';
-
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import cardReducer from './slices/cardSlice';
 
-const combineReducers= () =>({
+
+const combineReducers = combineReducers({
     card: cardReducer
 })
 
-const rootReducer = (state, action)=>{
-    if(action.type === "GET_CARD"){
-        return combineReducers(undefined, action)
-    }
-    // return combineReducers(state, action);
-}
 
 export default configureStore({
-    reducer: rootReducer,
+    reducer
 })
